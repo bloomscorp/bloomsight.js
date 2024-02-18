f9dfc712f41cb506413eca6c9a16b264bc310b9c
ayan <ayan@bloomscorp.com>
2024-02-18 09:17:39 +0530

    feat: add initial config support

---
+++ src/configuration/configuration.ts
@@ -0,0 +1,15 @@
+import {IConfig} from "./interface/config";
+
+export let config: IConfig | null = null;
+
+export function initConfig(appConfig: IConfig): void {
+	config = appConfig
+}
+
+export function resolvePropertyToken(): string {
+	return config ? config.propertyToken : '';
+}
+
+export function isDevelopmentMode(): boolean {
+	return config ? config.isDevelopmentMode : false;
+}
---
+++ src/configuration/interface/config.ts
@@ -0,0 +1,4 @@
+export interface IConfig {
+	propertyToken: string,
+	isDevelopmentMode: boolean
+}
