520d514de4402e6b46487495e603c2277dc59af3
ayan <ayan@bloomscorp.com>
2024-02-18 11:02:35 +0530

    feat: add location info interface

---
+++ src/location/interface/location-info.ts
@@ -0,0 +1,12 @@
+export interface ILocationInfo {
+	range: number[]
+	country: string
+	region: string
+	eu: string
+	timezone: string
+	city: string
+	ll: number[]
+	metro: number
+	area: number
+	ip: string
+}
