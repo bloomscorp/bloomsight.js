5fb4017dba0a231fff5cba2c5e26126d21dcf206
ayan <ayan@bloomscorp.com>
2024-02-18 11:01:38 +0530

    feat: add basic get http api

--- src/transmission/transmission.ts
+++ src/transmission/transmission.ts
@@ -1,12 +1,46 @@
-import {TransmissionResponse} from "./interface/transmission-response";
+import {ITransmissionResponse} from "./interface/transmission-response";
+import {isEmptyString} from "bmx-pastebox";
+import {RESOURCE_VALIDATION_ERROR} from "../support/message";
+
+export function executeBasicGet<T>(
+	url: string,
+	headers: Headers | undefined,
+	onPreExecute: () => void,
+	onPostExecute: (response: string) => void,
+	onSuccess: (response: string) => void,
+	onFailure: (error: string) => void,
+	onComplete: () => void,
+): void {
+
+	onPreExecute();
+
+	fetch(url, {
+		method: 'GET',
+		headers,
+	})
+		.then((response: Response) => response.json() as PromiseLike<ITransmissionResponse>)
+		.then(
+			(response: ITransmissionResponse): void => {
+				onPostExecute(response.message);
+				if (response.success) onSuccess(response.message);
+				else onFailure(RESOURCE_VALIDATION_ERROR);
+			}
+		)
+		.catch(
+			(error): void => onFailure(error.toString())
+		)
+		.finally(
+			(): void => onComplete()
+		);
+}
 
 export function executePostPayload<T>(
 	url: string,
 	payload: T,
 	headers: Headers | undefined,
 	onPreExecute: () => void,
-	onPostExecute: (response: TransmissionResponse) => void,
-	onSuccess: (response: TransmissionResponse) => void,
+	onPostExecute: (response: ITransmissionResponse) => void,
+	onSuccess: (response: ITransmissionResponse) => void,
 	onFailure: (error: string) => void,
 	onComplete: () => void,
 ): void {
@@ -18,11 +52,12 @@ export function executePostPayload<T>(
 		headers,
 		body: JSON.stringify(payload)
 	})
-		.then((response: Response) => response.json() as PromiseLike<TransmissionResponse>)
+		.then((response: Response) => response.json() as PromiseLike<ITransmissionResponse>)
 		.then(
-			(response: TransmissionResponse): void => {
+			(response: ITransmissionResponse): void => {
 				onPostExecute(response);
 				if (response.success) onSuccess(response);
+				else onFailure(isEmptyString(response.message) ? RESOURCE_VALIDATION_ERROR : response.message);
 			}
 		)
 		.catch(
