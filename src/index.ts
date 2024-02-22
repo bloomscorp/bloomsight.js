3c009c1cf67f2458c87924c261365cc06e8fe4f2
ayan <ayan@bloomscorp.com>
2024-02-22 15:46:51 +0530

    feat: add config to manage web api page view observer

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
 import {initPlatform} from "./platform/platform";
 import {initLocation} from "./location/location";
-import {isConfiguredProperly} from "./configuration/configuration";
 import {initUser} from "./user/user";
 import {initSession} from "./session/session";
-import {initPageViewEventHandler} from "./event/page-event";
+import {initPageViewEventHandler} from "./event/page-view-event";
+import {IConfig} from "./configuration/interface/config";
+import {initConfig} from "./configuration/configuration";
 
-(function init(): void {
-
-	if (!isConfiguredProperly()) {
-		console.log(`provide valid bloomsight configuration!`);
-		return;
-	}
+function init(appConfig: IConfig): void {
+	initConfig(appConfig);
 
 	initPlatform();
 	initLocation();
 	initUser();
 	initSession();
-	initPageViewEventHandler();
-})();
+
+	if (appConfig.observePageViaWebAPI) {
+		initPageViewEventHandler();
+	}
+}
+
+export {init};
+export {resolveSimpleEvent} from './event/simple-event';
+export {resolveDataEvent} from './event/data-event';
+export {pageViewObserver} from './event/page-view-event';
