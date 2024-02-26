dfa571c576bb505554380c108e2419e36d294026
ayan <ayan@bloomscorp.com>
2024-02-26 10:02:42 +0530

    test: add vitest as testing framework

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+// @ts-ignore
+import {defineConfig} from 'vitest/config';
+
+export default defineConfig({
+	test: {
+		browser: {
+			enabled: true,
+			name: 'chrome',
+			headless: true
+		}
+	},
+})
