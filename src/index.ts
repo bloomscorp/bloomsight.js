13e9f6ea9db979177fe9d64cbcc1f0bbf34e3af0
ayan <ayan@bloomscorp.com>
2024-04-30 23:38:55 +0530

    fix: remove metainfo restriction for sendEmail

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,28 @@ import {initPlatform} from "./platform/platform";
 import {initLocation} from "./location/location";
 import {initUser} from "./user/user";
 import {initSession} from "./session/session";
-import {initPageViewEventHandler} from "./event/page-view-event";
+import {initPageViewEventHandler, pageViewObserver} from "./event/page-view-event";
 import {IConfig} from "./configuration/interface/config";
 import {initConfig} from "./configuration/configuration";
 
 import {resolveSimpleEvent} from './event/simple-event';
 import {resolveDataEvent} from './event/data-event';
-import {pageViewObserver} from './event/page-view-event';
 import {sendEmail} from "./email/email";
 import {validateProperty} from "./property/property";
 import {isBrowser, resolveHost} from "./utils/browser-api";
 
 function init(appConfig: IConfig): void {
+
 	initConfig(appConfig);
 
-	validateProperty(
-		appConfig.propertyToken,
-		resolveHost()
-	)
+	validateProperty(appConfig.propertyToken, resolveHost())
 		.then((isValid: boolean): void => {
 
 			if (!isValid) {
 				console.error(`propertyToken is not valid for ${resolveHost()}`);
-				return;
+				appConfig.stopAll = true;
+			} else {
+				appConfig.stopAll = false;
 			}
 
 			initLocation((): void => {
